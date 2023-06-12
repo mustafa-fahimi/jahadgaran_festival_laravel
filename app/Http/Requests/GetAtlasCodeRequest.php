1b478421ac1fc827c131f00de3d9d68a443ac5e1
Tokenizer <mostafafahimi73@gmail.com>
2023-06-12 16:07:03 +0330

    refactor: request files and added failedValidation method to all of them

--- app/Http/Requests/GetAtlasCodeRequest.php
+++ app/Http/Requests/GetAtlasCodeRequest.php
@@ -2,27 +2,44 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use App\Traits\HttpResponses;
 
 class GetAtlasCodeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+  use HttpResponses;
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-          'group_supervisor_national_code' => ['required', 'string'],
-        ];
-    }
+  protected function failedValidation(Validator $validator)
+  {
+    $errors = $validator->errors()->toArray();
+    throw new HttpResponseException(
+      $this->error(
+        $errors,
+        message: 'اطلاعات ارسال شده صحیح نمی باشد',
+        code: Response::HTTP_UNPROCESSABLE_ENTITY,
+      ),
+    );
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'group_supervisor_national_code' => ['required', 'string'],
+    ];
+  }
 }
--- app/Http/Requests/JahadiGroupSubmittedWorksRequest.php
+++ app/Http/Requests/JahadiGroupSubmittedWorksRequest.php
@@ -2,30 +2,47 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use App\Traits\HttpResponses;
 
 class JahadiGroupSubmittedWorksRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+  use HttpResponses;
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'national_code' => ['required', 'string'],
-            'verify_code' => ['required', 'string'],
-            'attachment_type' => ['required', 'string'],
-            'file' => ['required', 'file'],
-        ];
-    }
+  protected function failedValidation(Validator $validator)
+  {
+    $errors = $validator->errors()->toArray();
+    throw new HttpResponseException(
+      $this->error(
+        $errors,
+        message: 'اطلاعات ارسال شده صحیح نمی باشد',
+        code: Response::HTTP_UNPROCESSABLE_ENTITY,
+      ),
+    );
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'national_code' => ['required', 'string'],
+      'verify_code' => ['required', 'string'],
+      'attachment_type' => ['required', 'string'],
+      'file' => ['required', 'file'],
+    ];
+  }
 }
